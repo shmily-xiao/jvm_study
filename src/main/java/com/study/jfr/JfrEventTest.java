83b383d9f8876b96c9957bc91d692b9ad277cd89
shmily <wangzaijun1234@126.com>
2021-08-03 00:20:07 +0800

    【add】study jvm： JFR event test

---
+++ src/main/java/com/study/jfr/JfrEventTest.java
@@ -0,0 +1,57 @@
+package com.study.jfr;
+
+import jdk.jfr.consumer.RecordedEvent;
+import jdk.jfr.consumer.RecordingFile;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.logging.Logger;
+
+public class JfrEventTest {
+
+    private static Predicate<RecordedEvent> testMaker(String s) {
+        return e -> e.getEventType().getName().startsWith(s);
+    }
+
+    private static final Map<Predicate<RecordedEvent>,
+            Function<RecordedEvent, Map<String, String>>> mappers =
+            Map.of(testMaker("ClassLoadingStatistics"),
+                    ev -> Map.of("start", ""+ ev.getStartTime(),
+                            "Loaded Class Count",""+ ev.getValue("loadedClassCount"),
+                            "Unloaded Class Count", ""+ ev.getValue("unloadedClassCount")
+                    ));
+
+    public void readJFRFile() throws IOException {
+        RecordingFile recordingFile = new RecordingFile(Paths.get("/Users/zaijunwang/workspace/java/jvm_study/myflight.jfr"));
+        while (recordingFile.hasMoreEvents()) {
+            RecordedEvent event = recordingFile.readEvent();
+            if (event != null) {
+                Map<String, String> details = convertEvent(event);
+                if (details == null) {
+                    // details为空
+                } else {
+                    // 打印目标
+                    System.out.println(details);
+                }
+            }
+        }
+    }
+
+    public Map<String, String> convertEvent(final RecordedEvent e) {
+        for (Map.Entry<Predicate<RecordedEvent>, Function<RecordedEvent, Map<String, String>>> ent : mappers.entrySet()) {
+            if (ent.getKey().test(e)) {
+                return ent.getValue().apply(e);
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) throws IOException {
+        JfrEventTest test = new JfrEventTest();
+        test.readJFRFile();
+    }
+}
--- src/main/java/com/study/jfr/JfrEvnetTest.java
+++
@@ -1,4 +0,0 @@
-package com.study.jfr;
-
-public class JfrEvnetTest {
-}
